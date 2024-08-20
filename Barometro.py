7d954bbef0e793b60bc5ea982439071553b1cebc
Patricia <79313546+pbarrios1783@users.noreply.github.com>
2024-08-20 11:13:40 +0200

    Update Barometro.py

--- Barometro.py
+++ Barometro.py
@@ -10,21 +10,21 @@ st.title("Barómetro de Data Abierta")
 
 # Barra lateral para seleccionar la capa
 st.sidebar.title("Selecciona una capa")
-Capa = st.sidebar.selectbox("Elige un dataset para mostrar", ['Capacidades', 'Gobernanza', 'Integridad Política'])
+capa = st.sidebar.selectbox("Elige un dataset para mostrar", ["Capacidades", "Gobernanza", "Integridad Política"])
 
 # Filtrar los datos según la capa seleccionada
-df_filtrado = df_final[df_final['Capa'] == Capa]
+df_filtrado = df_final[df_final["Capa"] == capa]
 
 # Barra lateral para deslizador de filtrado de puntuaciones
-min_score, max_score = st.sidebar.slider("Selecciona el rango de puntuación", float(df_filtrado['score'].min()), float(df_filtrado['score'].max()), (float(df_filtrado['score'].min()), float(df_filtrado['score'].max())))
-datos_filtrados = df_filtrado[(df_filtrado['score'] >= min_score) & (df_filtrado['score'] <= max_score)]
+min_score, max_score = st.sidebar.slider("Selecciona el rango de puntuación", float(df_filtrado["score"].min()), float(df_filtrado["score"].max()), (float(df_filtrado["score"].min()), float(df_filtrado["score"].max())))
+datos_filtrados = df_filtrado[(df_filtrado["score"] >= min_score) & (df_filtrado["score"] <= max_score)]
 
 # Crear el mapa coroplético de Plotly basado en la capa seleccionada y el rango de puntuaciones
 fig = px.choropleth(datos_filtrados, 
                     geojson="https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson", 
-                    locations='iso3',  
-                    color='score',  
-                    hover_name='country',  
+                    locations="iso3",  
+                    color="score",  
+                    hover_name="country",  
                     color_continuous_scale="Viridis", 
                     range_color=(min_score, max_score),  
                     labels={'score': "Puntuación"},  
